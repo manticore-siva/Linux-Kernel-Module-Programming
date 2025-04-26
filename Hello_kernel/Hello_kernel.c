249f7a14b55a76b039322ffd02b7cfe2b6066e1e
Manticore <vsiva5110@gmail.com.com>
2025-04-26 12:20:10 +0530

    Added Kernel Hellow world program

---
+++ Hello_kernel/Hello_kernel.c
@@ -0,0 +1,19 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");  // This line is mandatory
+MODULE_AUTHOR("Manticore");
+MODULE_DESCRIPTION("A Sample Kernel Module for Test purpose");
+MODULE_VERSION("v_0.1");
+
+int init_module(void)
+{
+    	printk(KERN_INFO "Hello World \n");
+	return 0;
+}
+
+void cleanup_module(void)
+{
+    	printk(KERN_INFO "Good bye world\n");
+}
